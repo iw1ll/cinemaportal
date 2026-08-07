1d19bfbf7aba8ec76ade30e446a3f04bd9e26e10
iWill <alxs1dd93@gmail.com>
2026-08-07 16:03:28 +0700

    fix + styles

--- src/app/project/layout/header/header.ts
+++ src/app/project/layout/header/header.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink,],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
