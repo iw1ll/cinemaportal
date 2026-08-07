4c972be2b4f29151ad660ed8ae2dafa692876ca0
iWill <alxs1dd93@gmail.com>
2026-08-07 15:49:30 +0700

    upd routing and core

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,18 +4,20 @@ import { SeriesComponent } from './project/components/series/series';
 import { AnimeComponent } from './project/components/anime/anime';
 import { CartoonsComponent } from './project/components/cartoons/cartoons';
 import { LayoutComponent } from './project/layout/layout/layout';
+import { MainComponent } from './project/components/main/main';
 
 export const routes: Routes = [
   {
     path: '',
     component: LayoutComponent,
     children: [
-      { path: '', redirectTo: '/movies', pathMatch: 'full' },
+      { path: '', redirectTo: '/main', pathMatch: 'full' },
+      { path: 'main', component: MainComponent },
       { path: 'movies', component: MoviesComponent },
       { path: 'series', component: SeriesComponent },
       { path: 'anime', component: AnimeComponent },
       { path: 'cartoons', component: CartoonsComponent },
-      { path: '**', redirectTo: '/movies' },
+      { path: '**', redirectTo: '/main' },
     ],
   },
 ];
