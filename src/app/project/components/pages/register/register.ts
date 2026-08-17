4f3cd9669597423dbf33500bc576fe2d0e86f9dc
iWill <alxs1dd93@gmail.com>
2026-08-17 16:46:43 +0700

    add register

--- src/app/project/components/pages/register/register.ts
+++ src/app/project/components/pages/register/register.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  FormArray,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -11,11 +17,32 @@ export class RegisterComponent {
   EMAIL_PATTERN = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
   registerForm = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.minLength(2)]),
     email: new FormControl('', [Validators.required, Validators.pattern(this.EMAIL_PATTERN)]),
-    password: new FormControl('', [Validators.required, Validators.minLength(8)])
+    password: new FormControl('', [Validators.required, Validators.minLength(8)]),
+    phones: new FormArray([new FormControl('', Validators.required)]),
   });
 
-  onSubmit() {
-    console.log(this.registerForm.value);
+  /** Получить FormArray телефонов */
+  get phones(): FormArray {
+    return this.registerForm.get('phones') as FormArray;
+  }
+
+  /** Добавить телефон */
+  addPhone(): void {
+    this.phones.push(new FormControl('', Validators.required));
+  }
+
+  /** Удалить телефон по индексу */
+  removePhone(index: number): void {
+    if (this.phones.length > 1) {
+      this.phones.removeAt(index);
+    }
+  }
+
+  onSubmit(): void {
+    if (this.registerForm.valid) {
+      console.log(this.registerForm.value);
+    }
   }
 }
