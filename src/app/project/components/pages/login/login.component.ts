50beefac7ba8a30de062c43f862fbec95e5d14ab
iWill <alxs1dd93@gmail.com>
2026-08-20 16:02:26 +0700

    fix

--- src/app/project/components/pages/login/login.component.ts
+++ src/app/project/components/pages/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, inject } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AuthService } from '../../../../shared/services/auth.service';
 import { Router } from '@angular/router';
+import { catchError, of, tap } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -11,27 +12,35 @@ import { Router } from '@angular/router';
   styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  /** Сервис для запросов авторизации на бэк */
   private authService = inject(AuthService);
+  /** Роутер для программной навигации */
   private router = inject(Router);
 
+  /** Форма логина */
   loginForm = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.email]),
     password: new FormControl('', [Validators.required, Validators.minLength(8)]),
   });
 
+  /** Отправка формы */
   onSubmit(): void {
     if (this.loginForm.invalid) return;
 
     const { email, password } = this.loginForm.value;
 
-    this.authService.login({ email: email ?? '', password: password ?? '' }).subscribe({
-      next: response => {
+    this.authService.login({
+      email: email ?? '',
+      password: password ?? '',
+    }).pipe(
+      tap((response) => {
         localStorage.setItem('token', response.token);
         this.router.navigate(['/top']);
-      },
-      error: err => {
+      }),
+      catchError((err) => {
         console.error('Ошибка логина:', err);
-      },
-    });
+        return of(null);
+      })
+    ).subscribe();
   }
 }
