ce430ace54b50dbda67fb9c81f396d05e048ba41
iWill <alxs1dd93@gmail.com>
2026-08-23 23:10:43 +0700

    add comment

--- src/app/shared/guards/auth.guard.ts
+++ src/app/shared/guards/auth.guard.ts
@@ -1,8 +1,13 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 
+/**
+ * Guard аутентификации для защиты маршрутов от неавторизованного доступа.
+ */
 export const authGuard: CanActivateFn = () => {
+  /** Router для навигации */
   const router = inject(Router);
+  /** Токен аутентификации  */
   const token = localStorage.getItem('token');
 
   if (token) {
